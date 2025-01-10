f3de5c41694ae00b0f5fc696e2aafba236b28a46
mohammad-damra <mohammad.s.damra@gmail.com>
2025-01-11 02:40:20 +0300

    node assignment

---
+++ app.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  //part 1
+
+  // res.writeHead(200,{"content-type":'text/html'})
+  // res.write("hello from the server this is a node js course ")
+
+  // res.end("./contactUs.html")
+
+  // part 2
+
+  fs.readFile("index.html", (err, data) => {
+    if (err) {
+      res.writeHead(500, { "content-type": "text/plain" });
+      console.log("sorry can't read from the server");
+    } else {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.end(data);
+    }
+  });
+});
+fs.writeFile("myText.txt", "My name is Mohammad", (err) => {
+  if (err) throw err;
+  console.log("Saved!");
+});
+
+
+const thePort = 3000;
+
+const HOST = "127.0.0.1";
+
+server.listen(thePort, HOST, () => {
+  console.log(
+    `the server is now running on port # ${thePort} and host ${HOST}`
+  );
+});
